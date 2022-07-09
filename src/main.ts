07f28e42d2d6b4dd10d603b2fa51affa8d549cd8
nadiaH <nadiahrb79@gmailcom>
2022-07-09 21:29:49 +0300

    users, jwt

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,16 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
-// import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app: NestExpressApplication = await NestFactory.create(AppModule);
 
-  // const config = new DocumentBuilder()
-  //   .setTitle('Auth-Server example')
-  //   .setDescription('The auth-server API description')
-  //   .setVersion('1.0')
-  //   .build();
-  // const document = SwaggerModule.createDocument(app, config);
-  // SwaggerModule.setup('/', app, document);
-
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   app.use(cookieParser());
 
-  await app.listen(3000);
+  await app.listen(5000);
 }
 bootstrap();
